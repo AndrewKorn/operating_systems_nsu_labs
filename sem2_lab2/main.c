8d8ca7412d08c67462fecac12c2b7082257ca5c4
andrew <a.korneshchuk1@g.nsu.ru>
2022-09-13 23:17:16 +0700

    add strerror

--- sem2_lab2/main.c
+++ sem2_lab2/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
+#include <string.h>
 
 void* new_thread_work(void* args) {
     for (int i = 0; i < 10; ++i) {
@@ -13,12 +13,12 @@ int main() {
 
     int status;
     if ((status = pthread_create(&tid, NULL, new_thread_work, NULL)) != 0) {
-        fprintf(stderr, "can't create thread, status =  %d\n", status);
+        fprintf(stderr, "can't create thread, status =  %s\n", strerror(status));
         return 1;
     }
 
     if ((status = pthread_join(tid, NULL)) != 0) {
-        fprintf(stderr, "can't join, status =  %d\n", status);
+        fprintf(stderr, "can't join, status =  %s\n", strerror(status));
         return 1;
     }
 
