a15e9161a1e732996979e600fcfceb4308e7a3f1
andrew <a.korneshchuk1@g.nsu.ru>
2022-09-13 23:26:15 +0700

    add strerror

--- sem2_lab3/main.c
+++ sem2_lab3/main.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 
 void* new_thread_work(void* args) {
     printf("%s\n", (char*)args);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        fprintf(stderr, "invalid number of arguments, needs 4\n");
-        return 1;
-    }
-
+int main() {
+    char* lines[4] = {"First line", "Second line", "Third line", "Fourth line"};
     pthread_t tid[4];
 
     int status;
     for (int i = 0; i < 4; ++i) {
-        if ((status = pthread_create(&tid[i], NULL, new_thread_work, argv[i + 1]) )!= 0) {
-            fprintf(stderr, "can't create thread %d, status =  %d\n", i, status);
+        if ((status = pthread_create(&tid[i], NULL, new_thread_work, lines[i]) )!= 0) {
+            fprintf(stderr, "can't create thread %d, status =  %s\n", i, strerror(status));
             return 1;
         }
     }
 
     for (int i = 0; i < 4; ++i) {
         if ((status = pthread_join(tid[i], NULL)) != 0) {
-            fprintf(stderr, "can't join %d, status = %d\n", i, status);
+            fprintf(stderr, "can't join %d, status = %s\n", i, strerror(status));
             return 1;
         }
     }
