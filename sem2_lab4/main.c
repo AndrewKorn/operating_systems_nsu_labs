7d8ea151e5553f626eea8842c870ed8377550444
andrew <a.korneshchuk1@g.nsu.ru>
2022-09-13 23:29:23 +0700

    add strerror

--- sem2_lab4/main.c
+++ sem2_lab4/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-
+#include <string.h>
 
 void* new_thread_func(void* args) {
     while(1) {
@@ -14,16 +14,16 @@ int main() {
 
     int status;
     if ((status = pthread_create(&tid, NULL, new_thread_func, NULL)) != 0) {
-        fprintf(stderr, "can't create thread, status = %d\n", status);
+        fprintf(stderr, "can't create thread, status = %s\n", strerror(status));
         return 1;
     }
 
     sleep(2);
 
     if ((status = pthread_cancel(tid)) != 0) {
-        fprintf(stderr, "can`t cancel thread, status = %d\n", status);
+        fprintf(stderr, "can`t cancel thread, status = %s\n", strerror(status));
     }
 
     printf("Hello, World!\n");
-    thread_exit(NULL);
+    return 0;
 }
